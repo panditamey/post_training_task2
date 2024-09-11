6cf71ce7ad5a90e5c916affff5bec24cfff89813
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-09-11 11:31:56 +0530

    Players Output DOne

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,128 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Sportz Interactive</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <select v-model="selectedTeam" @change="filterPlayers">
+              <option value="ALL">ALL</option>
+              <option value="ALL">ALL</option>
+              <option value="IND">IND</option>
+              <option value="PAK">PAK</option>
+              <option value="AUS">AUS</option>
+              <option value="ENG">ENG</option>
+              <option value="ENG">ENG</option>
+              <option value="ENG">ENG</option>
+            </select>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container text-center">
+      <div class="row batsman-row">
+        <h1>Batsmen</h1>
+        <div class="col" v-for="player in batsmen" :key="player.name">
+          <div class="card">
+            <img :src="player.image" class="card-img-top" alt="Player Image" />
+            <div class="card-body">
+              <h5 class="card-title">{{ player.name }}</h5>
+              <p class="card-text">Matches: {{ player.matches }}</p>
+              <p class="card-text">Runs: {{ player.runs }}</p>
+              <p class="card-text">
+                50/100s: {{ player["50s"] }}/{{ player["100s"] }}
+              </p>
+              <p class="card-text">Highest Score: {{ player.highest_score }}</p>
+              <p class="card-text">Team Name: {{ player.team_name }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row bowler-row">
+        <h1>Bowlers</h1>
+        <div class="col" v-for="player in bowlers" :key="player.name">
+          <div class="card">
+            <img :src="player.image" class="card-img-top" alt="Player Image" />
+            <div class="card-body">
+              <h5 class="card-title">{{ player.name }}</h5>
+              <p class="card-text">Matches: {{ player.matches }}</p>
+              <p class="card-text">
+                Best Bowling Figures: {{ player.best_bowling_figures }}
+              </p>
+              <p class="card-text">Team Name: {{ player.team_name }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row all-rounder-row">
+        <h1>All Rounders</h1>
+        <div class="col" v-for="player in allRounders" :key="player.name">
+          <div class="card">
+            <img :src="player.image" class="card-img-top" alt="Player Image" />
+            <div class="card-body">
+              <h5 class="card-title">{{ player.name }}</h5>
+              <p class="card-text">Matches: {{ player.matches }}</p>
+              <p class="card-text">Runs: {{ player.runs }}</p>
+              <p class="card-text">
+                Best Bowling Figures: {{ player.best_bowling_figures }}
+              </p>
+              <p class="card-text">Team Name: {{ player.team_name }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import playersData from "./players.json";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      selectedTeam: "ALL",
+      players: playersData.originalPlayers,
+      filteredPlayers: playersData.originalPlayers,
+    };
+  },
+  computed: {
+    batsmen() {
+      return this.filteredPlayers.filter((player) => player.role === 2);
+    },
+    bowlers() {
+      return this.filteredPlayers.filter((player) => player.role === 4);
+    },
+    allRounders() {
+      return this.filteredPlayers.filter((player) => player.role === 3);
+    },
+  },
+  methods: {
+    filterPlayers() {
+      if (this.selectedTeam === "ALL") {
+        this.filteredPlayers = this.players;
+      } else {
+        this.filteredPlayers = this.players.filter(
+          (player) => player.team_name === this.selectedTeam
+        );
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
